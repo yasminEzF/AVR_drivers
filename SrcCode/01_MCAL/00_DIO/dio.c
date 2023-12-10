cf4fc370357107b14f208f88077b0069c2bb7e8e
Yasmin <yasminfayed00@gmail.com>
2023-12-10 23:36:10 +0200

    port config + port drivers upload, updated dio

--- SrcCode/01_MCAL/00_DIO/dio.c
+++ SrcCode/01_MCAL/00_DIO/dio.c
@@ -1,11 +1,12 @@
 #include "bit_math.h"
 #include "std_types.h"
 #include "AVR_reg.h"
+#include "portCfg.h"
 #include "dio.h"
 
 DIO_enumError_t DIO_enumSetPinConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_t Copy_enumPinNum,DIO_enumConfig_t Copy_enumConfig) {
 	DIO_enumError_t errorStatus = DIO_enumNotOk;
-	if(Copy_enumPortNum > DIO_enumPortD) {
+	if(Copy_enumPortNum > PORTS_NUM) {
 		errorStatus = DIO_enumWrongPort;
 	}
 	else if(Copy_enumConfig > DIO_enumInputExternalPD) {
@@ -18,39 +19,51 @@ DIO_enumError_t DIO_enumSetPinConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPi
 		errorStatus = DIO_enumOk;
 		volatile u8 *DDR_regPtr = NULL, *PORT_regPtr = NULL;
 		switch(Copy_enumPortNum) {
+#if ((PORTS_NUM == PORT_A)||(PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortA:
-			DDR_regPtr = DDRA;
-			PORT_regPtr = PORTA;
-			break;
+				DDR_regPtr = DDRA_REG;
+				PORT_regPtr = PORTA_REG;
+				break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortB:
-			DDR_regPtr = DDRB;
-			PORT_regPtr = PORTB;
+				DDR_regPtr = DDRB_REG;
+				PORT_regPtr = PORTB_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortC:
-			DDR_regPtr = DDRC;
-			PORT_regPtr = PORTC;
+				DDR_regPtr = DDRC_REG;
+				PORT_regPtr = PORTC_REG;
 			break;
+#endif
+#if (PORTS_NUM == PORTS_A_B_C_D)
 			case DIO_enumPortD:
-			DDR_regPtr = DDRD;
-			PORT_regPtr = PORTD;
+				DDR_regPtr = DDRD_REG;
+				PORT_regPtr = PORTD_REG;
 			break;
+#endif
+			default:
+                    errorStatus = DIO_enumWrongPort;
 		}
 		if((DDR_regPtr == NULL) || (PORT_regPtr == NULL)){
-			errorStatus = DIO_enumNullPointer;
+			errorStatus = DIO_enumWrongPort;
 		}
 		else {
 			switch(Copy_enumConfig) {
 				case DIO_enumOutput:
-				SET_BIT(*DDR_regPtr,Copy_enumPinNum);
+					SET_BIT(*DDR_regPtr,Copy_enumPinNum);
 				break;
 				case DIO_enumInputInternalPU:
-				CLR_BIT(*DDR_regPtr, Copy_enumPinNum);
-				SET_BIT(*PORT_regPtr, Copy_enumPinNum);
+					CLR_BIT(*DDR_regPtr, Copy_enumPinNum);
+					SET_BIT(*PORT_regPtr, Copy_enumPinNum);
 				break;
 				case DIO_enumInputExternalPD:
-				CLR_BIT(*DDR_regPtr, Copy_enumPinNum);
-				CLR_BIT(*PORT_regPtr, Copy_enumPinNum);
+					CLR_BIT(*DDR_regPtr, Copy_enumPinNum);
+					CLR_BIT(*PORT_regPtr, Copy_enumPinNum);
 				break;
+				default:
+					errorStatus = DIO_enumWrongConfig;
 			}
 		}
 	}
@@ -59,7 +72,7 @@ DIO_enumError_t DIO_enumSetPinConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPi
 
 DIO_enumError_t DIO_enumSetPortConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumConfig_t Copy_enumConfig) {
 	DIO_enumError_t errorStatus = DIO_enumNotOk;
-	if(Copy_enumPortNum > DIO_enumPortD) {
+	if(Copy_enumPortNum > PORTS_NUM) {
 		errorStatus = DIO_enumWrongPort;
 	}
 	else if(Copy_enumConfig > DIO_enumInputExternalPD) {
@@ -69,39 +82,51 @@ DIO_enumError_t DIO_enumSetPortConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumC
 		errorStatus = DIO_enumOk;
 		volatile u8 *DDR_regPtr = NULL, *PORT_regPtr = NULL;
 		switch(Copy_enumPortNum) {
+#if ((PORTS_NUM == PORT_A)||(PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortA:
-			DDR_regPtr = DDRA;
-			PORT_regPtr = PORTA;
+				DDR_regPtr = DDRA_REG;
+				PORT_regPtr = PORTA_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortB:
-			DDR_regPtr = DDRB;
-			PORT_regPtr = PORTB;
+				DDR_regPtr = DDRB_REG;
+				PORT_regPtr = PORTB_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortC:
-			DDR_regPtr = DDRC;
-			PORT_regPtr = PORTC;
+				DDR_regPtr = DDRC_REG;
+				PORT_regPtr = PORTC_REG;
 			break;
+#endif
+#if (PORTS_NUM == PORTS_A_B_C_D)
 			case DIO_enumPortD:
-			DDR_regPtr = DDRD;
-			PORT_regPtr = PORTD;
+				DDR_regPtr = DDRD_REG;
+				PORT_regPtr = PORTD_REG;
 			break;
+#endif
+			default:
+                    errorStatus = DIO_enumWrongPort;
 		}
 		if((DDR_regPtr == NULL) || (PORT_regPtr == NULL)){
-			errorStatus = DIO_enumNullPointer;
+			errorStatus = DIO_enumWrongPort;
 		}
 		else {
 			switch(Copy_enumConfig) {
 				case DIO_enumOutput:
-				*DDR_regPtr = 0xFF;
+					*DDR_regPtr = 0xFF;
 				break;
 				case DIO_enumInputInternalPU:
-				*DDR_regPtr = 0x00;
-				*PORT_regPtr = 0xFF;
+					*DDR_regPtr = 0x00;
+					*PORT_regPtr = 0xFF;
 				break;
 				case DIO_enumInputExternalPD:
-				*DDR_regPtr = 0x00;
-				*PORT_regPtr = 0x00;
+					*DDR_regPtr = 0x00;
+					*PORT_regPtr = 0x00;
 				break;
+				default:
+					errorStatus = DIO_enumWrongConfig;
 			}
 		}
 	}
@@ -110,7 +135,7 @@ DIO_enumError_t DIO_enumSetPortConfig(DIO_enumPorts_t Copy_enumPortNum,DIO_enumC
 
 DIO_enumError_t DIO_enumSetPin(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_t Copy_enumPinNum,DIO_enumLogicState_t Copy_enumLogicState) {
 	DIO_enumError_t errorStatus = DIO_enumNotOk;
-	if(Copy_enumPortNum > DIO_enumPortD) {
+	if(Copy_enumPortNum > PORTS_NUM) {
 		errorStatus = DIO_enumWrongPort;
 	}
 	else if(Copy_enumPinNum > DIO_enumPin7) {
@@ -123,30 +148,42 @@ DIO_enumError_t DIO_enumSetPin(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_t C
 		errorStatus = DIO_enumOk;
 		volatile u8 *PORT_regPtr = NULL;
 		switch(Copy_enumPortNum) {
+#if ((PORTS_NUM == PORT_A)||(PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortA:
-			PORT_regPtr = PORTA;
+				PORT_regPtr = PORTA_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortB:
-			PORT_regPtr = PORTB;
+				PORT_regPtr = PORTB_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortC:
-			PORT_regPtr = PORTC;
+				PORT_regPtr = PORTC_REG;
 			break;
+#endif
+#if (PORTS_NUM == PORTS_A_B_C_D)
 			case DIO_enumPortD:
-			PORT_regPtr = PORTD;
+				PORT_regPtr = PORTD_REG;
 			break;
+#endif
+			default:
+                    errorStatus = DIO_enumWrongPort;
 		}
 		if(PORT_regPtr == NULL) {
-			errorStatus = DIO_enumNullPointer;
+			errorStatus = DIO_enumWrongPort;
 		}
 		else {
 			switch(Copy_enumLogicState) {
 				case DIO_enumLogicHigh:
-				SET_BIT(*PORT_regPtr, Copy_enumPinNum);
+					SET_BIT(*PORT_regPtr, Copy_enumPinNum);
 				break;
 				case DIO_enumLogicLow:
-				CLR_BIT(*PORT_regPtr, Copy_enumPinNum);
+					CLR_BIT(*PORT_regPtr, Copy_enumPinNum);
 				break;
+				default:
+					errorStatus = DIO_enumWrongPinState;
 			}
 		}
 	}
@@ -155,7 +192,7 @@ DIO_enumError_t DIO_enumSetPin(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_t C
 
 DIO_enumError_t DIO_enumReadState(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_t Copy_enumPinNum,DIO_enumLogicState_t* Add_Pu8PinValue) {
 	DIO_enumError_t errorStatus = DIO_enumNotOk;
-	if(Copy_enumPortNum > DIO_enumPortD) {
+	if(Copy_enumPortNum > PORTS_NUM) {
 		errorStatus = DIO_enumWrongPort;
 	}
 	else if(Copy_enumPinNum > DIO_enumPin7) {
@@ -165,24 +202,34 @@ DIO_enumError_t DIO_enumReadState(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_
 		errorStatus = DIO_enumOk;
 		volatile u8 *PIN_regPtr = NULL;
 		switch(Copy_enumPortNum) {
+#if ((PORTS_NUM == PORT_A)||(PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortA:
-			PIN_regPtr = PINA;
+				PIN_regPtr = PINA_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortB:
-			PIN_regPtr = PINB;
+				PIN_regPtr = PINB_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortC:
-			PIN_regPtr = PINC;
+				PIN_regPtr = PINC_REG;
 			break;
+#endif
+#if (PORTS_NUM == PORTS_A_B_C_D)
 			case DIO_enumPortD:
-			PIN_regPtr = PIND;
+				PIN_regPtr = PIND_REG;
 			break;
+#endif
+			default:
+                    errorStatus = DIO_enumWrongPort;
 		}
 		if(PIN_regPtr == NULL) {
-			errorStatus = DIO_enumNullPointer;
+			errorStatus = DIO_enumWrongPort;
 		}
 		else {
-			*Add_Pu8PinValue = (char)GET_BIT(*PIN_regPtr,(char)Copy_enumPinNum);
+			*Add_Pu8PinValue = (u8)GET_BIT(*PIN_regPtr,(u8)Copy_enumPinNum);
 		}
 	}
 	return errorStatus;
@@ -190,7 +237,7 @@ DIO_enumError_t DIO_enumReadState(DIO_enumPorts_t Copy_enumPortNum,DIO_enumPins_
 
 DIO_enumError_t DIO_enumSetPort(DIO_enumPorts_t Copy_enumPortNum,DIO_enumLogicState_t Copy_enumLogicState) {
 	DIO_enumError_t errorStatus = DIO_enumNotOk;
-	if(Copy_enumPortNum > DIO_enumPortD) {
+	if(Copy_enumPortNum > PORTS_NUM) {
 		errorStatus = DIO_enumWrongPort;
 	}
 	else if(Copy_enumLogicState > DIO_enumLogicHigh) {
@@ -200,35 +247,48 @@ DIO_enumError_t DIO_enumSetPort(DIO_enumPorts_t Copy_enumPortNum,DIO_enumLogicSt
 		errorStatus = DIO_enumOk;
 		volatile u8 *PORT_regPtr = NULL;
 		switch(Copy_enumPortNum) {
+#if ((PORTS_NUM == PORT_A)||(PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortA:
-			PORT_regPtr = PORTA;
+				PORT_regPtr = PORTA_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B)||(PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortB:
-			PORT_regPtr = PORTB;
+				PORT_regPtr = PORTB_REG;
 			break;
+#endif
+#if ((PORTS_NUM == PORTS_A_B_C)||(PORTS_NUM == PORTS_A_B_C_D))
 			case DIO_enumPortC:
-			PORT_regPtr = PORTC;
+				PORT_regPtr = PORTC_REG;
 			break;
+#endif
+#if (PORTS_NUM == PORTS_A_B_C_D)
 			case DIO_enumPortD:
-			PORT_regPtr = PORTD;
+				PORT_regPtr = PORTD_REG;
 			break;
+#endif
+			default:
+                    errorStatus = DIO_enumWrongPort;
 		}
 		if(PORT_regPtr == NULL) {
-			errorStatus = DIO_enumNullPointer;
+			errorStatus = DIO_enumWrongPort;
 		}
 		else {
 			switch(Copy_enumLogicState) {
 				case DIO_enumLogicHigh:
-				ASSIGN_REG(*PORT_regPtr, 0xFF);
+					ASSIGN_REG(*PORT_regPtr, 0xFF);
 				break;
 				case DIO_enumLogicLow:
-				ASSIGN_REG(*PORT_regPtr, 0x00);
+					ASSIGN_REG(*PORT_regPtr, 0x00);
 				break;
+				default:
+					errorStatus = DIO_enumWrongPinState;
 			}
 		}
 	}
 	return errorStatus;
 }
+
 void DIO_DelayMs(u32 Copy_u32TimeMs) {
 	// Assembly code auto-generated
 	// by utility from Bret Mulvey
---
+++ SrcCode/01_MCAL/01_PORT/port.h
@@ -0,0 +1,6 @@
+#ifndef PORT_H_
+#define PORT_H_
+
+void portInit(void);
+
+#endif /* PORT_H_ */
